d5d5b41d07ae39082bfd882c4575aba6dbe3c883
tinalin0 <112659515+tinalin0@users.noreply.github.com>
2022-12-09 13:42:40 -0700

    Update flashcard-storage-array.js

--- js/flashcard-storage-array.js
+++ js/flashcard-storage-array.js
@@ -15,3 +15,27 @@ function loadDecksPriv() {
   let privateStr = localStorage.getItem('privateArray');
   return JSON.parse(privateStr) ?? [];
 }
+
+
+// Display all the public Sets
+function displayAllPub(array) {
+  for (let i = 0; i < array.length; i++) {
+
+  }
+}
+
+// Helper Functions
+// Loop though all the Arrays
+
+// Create Elements to Display them in
+
+
+// Display Private Sets of the User
+
+
+// Helper Functions
+// Loop through all of the Arrays
+
+// Find the One with the right User
+
+// Create element to display it in
