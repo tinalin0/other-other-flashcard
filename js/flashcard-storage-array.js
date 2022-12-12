9aae5d11c9034f5665f6029a304b5625caf814aa
tinalin0 <112659515+tinalin0@users.noreply.github.com>
2022-12-12 14:05:08 -0700

    s

--- js/flashcard-storage-array.js
+++ js/flashcard-storage-array.js
@@ -16,17 +16,35 @@ function loadDecksPriv() {
   return JSON.parse(privateStr) ?? [];
 }
 
+// Variables
+let publicDiv = document.getElementById("public-sets");
+let privateSets = document.getElementById("private-sets");
+
 // Display all the Sets
 function displayAllSets(array) {
   createDisplay();
   for (let i = 0; i < array.length; i++) {
     for (let t = 0; t < array[i].set.length; t++) {
-      
+      createContainer(t, array[i].set[t].term, array[i].set[t].def);
     }
   }
 }
 
 // Helper Functions
+function createDisplay() {
+  // Create Elements to Display them in
+  
+
+  // Loop though all the Arrays
+
+  // Set Array Variables as Elements
+}
+
+function createContainer(i, term, definition) {
+
+}
+
+// Helper Functions - Private
 function findArraysWithUser(array) {
   let userSetsArray = [];
   // Loop through all of the Arrays
@@ -37,14 +55,4 @@ function findArraysWithUser(array) {
     }
   }
   displayAllSets(userSetsArray);
-}
-
-// Helper Functions
-function createDisplay() {
-  // Create Elements to Display them in
-  
-
-  // Loop though all the Arrays
-
-// Set Array Variables as Elements
 }
