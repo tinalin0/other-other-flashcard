635b8e85f76b77ab1818ea9eaac5447fae39d14a
tinalin0 <112659515+tinalin0@users.noreply.github.com>
2023-01-05 12:24:37 -0700

    Update flashcard-storage-array.js

--- js/flashcard-storage-array.js
+++ js/flashcard-storage-array.js
@@ -10,18 +10,18 @@ function searchPriv() {
 }
 
 function saveDecksPub() {
-  localStorage.setItem("publicArray", JSON.stringify(publicArray))
+  localStorage.setItem('publicArray', JSON.stringify(publicArray))
 }
-  
+
 function loadDecksPub() {
   let publicStr = localStorage.getItem('publicArray');
   return JSON.parse(publicStr) ?? [];
 }
 
 function saveDecksPriv() {
-  localStorage.setItem("privateArray", JSON.stringify(privateArray))
+  localStorage.setItem('privateArray', JSON.stringify(privateArray))
 }
-  
+
 function loadDecksPriv() {
   let privateStr = localStorage.getItem('privateArray');
   return JSON.parse(privateStr) ?? [];
